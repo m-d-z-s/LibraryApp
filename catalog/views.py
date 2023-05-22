fa56112f46b0993d5536a0d51891afe4b5765a7a
BrusilovaIV <ira.brusilova@list.ru>
2023-05-22 09:23:38 +0300

    completed part 7

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre
 
@@ -15,12 +16,18 @@ def index(request):
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    # Render the HTML template index.html with the data in the context variable.
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},  # num_visits appended
     )
-from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
@@ -35,3 +42,4 @@ class AuthorListView(generic.ListView):
     """Generic class-based list view for a list of authors."""
     model = Author
     paginate_by = 10
+
